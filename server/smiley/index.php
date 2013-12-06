2716221e99366268931aeec1d3d6417485dbc0a7
tuj <tujensen@gmail.com>
2013-12-06 13:38:42 +0100

    server code, version 1

--- server/smiley/index.php
+++ server/smiley/index.php
@@ -2,34 +2,36 @@
 	header('Access-Control-Allow-Origin: *');
 
 	include_once dirname(__FILE__).'/utils/utils.php.inc';
-	include_once dirname(__FILE__).'/database/smiley.php.inc';
+	include_once dirname(__FILE__).'/database/smileydb.php.inc';
 	
 	$action = Utils::getParam("action");
-	$smiley = new Smiley();
+	$smileyDB = new SmileyDB();
 	
 	switch ($action) {
-    case "login":
-		$id = Utils::getParam("id");
-	
-		$smiley->login($id);
-        break;
-    case "register":
-		$contact = Utils::getParam("contact");
-		$mail    = Utils::getParam("mail");
-		$magafd  = Utils::getParam("magafd");
-		$forvalt = Utils::getParam("forvalt");
-		$place   = Utils::getParam("place");
-		$name    = Utils::getParam("name");
+		case "login":
+			$macid = Utils::getParam("macid");
+		
+			$smileyDB->login($macid);
+			break;
+		case "register":
+			$contact = Utils::getParam("contact");
+			$mail    = Utils::getParam("mail");
+			$magafd  = Utils::getParam("magafd");
+			$forvalt = Utils::getParam("forvalt");
+			$place   = Utils::getParam("place");
+			$name    = Utils::getParam("name");
 
-		$smiley->register($contact, $mail, $magafd, $forvalt, $place, $name);		
-		break;
-	case "result":
-		$id     = Utils::getParam("id");
-		$smiley = Utils::getParam("smiley");
-		$what   = Utils::getParam("what");
-		$date   = Utils::getParam("date");
-	
-		$smiley->result($id, $date, $smiley, $what);
-        break;
+			$smileyDB->register($contact, $mail, $magafd, $forvalt, $place, $name);		
+			break;
+		case "result":
+			$macid    = Utils::getParam("macid");
+			$datetime = Utils::getParam("datetime");
+			$smiley   = Utils::getParam("smiley");
+			$what     = Utils::getParam("what");
+		
+			$smileyDB->insertResult($macid, $datetime, $smiley, $what);
+			break;
+		default:
+			break;
 	}
 ?>
