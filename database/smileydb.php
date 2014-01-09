e67e75f3479d7d62b2ead190c38e118ad20e6b0c
Troels Ugilt Jensen <tujensen@gmail.com>
2014-01-09 14:52:26 +0100

    indention

--- database/smileydb.php
+++ database/smileydb.php
@@ -15,15 +15,15 @@ class SmileyDB {
    * Constructor
    */
   public function __construct() {
-		$this->connection = PDOMysql::getInstance();
-	}
+    $this->connection = PDOMysql::getInstance();
+  }
 
   /**
    * Destructor
    */
   public function __destruct() {
-		$this->connection = null;
-	}
+    $this->connection = null;
+  }
 
   /**
    * Check that the machine has been registered. Outputs the result as json.
@@ -31,21 +31,21 @@ class SmileyDB {
    */
   public function login($macid) {
     // Get machines with macid = $macid.
-		$statement = 'SELECT * FROM machine WHERE macid = :macid';
-		$query = $this->connection->execute($statement, array('macid' => $macid));
+    $statement = 'SELECT * FROM machine WHERE macid = :macid';
+    $query = $this->connection->execute($statement, array('macid' => $macid));
 
     // Count the number of hits. If one then success, else error.
-		$count = $query->rowCount();
-		if ($count == 1) {		// machine exists
-			$result = array('result'=>'ok');
-			echo json_encode($result);
-			return;
-		} else {
-			$result = array('result'=>'error', 'msg'=>'error_wrong_id');
-			echo json_encode($result);
-			return;
-		}
-	}
+    $count = $query->rowCount();
+    if ($count == 1) {		// machine exists
+      $result = array('result'=>'ok');
+      echo json_encode($result);
+      return;
+    } else {
+      $result = array('result'=>'error', 'msg'=>'error_wrong_id');
+      echo json_encode($result);
+      return;
+    }
+  }
 
   /**
    * Register a new machine with the given parameters. Outputs the macid as json.
@@ -57,84 +57,86 @@ class SmileyDB {
    * @param $name device name.
    */
   public function register($contact, $mail, $magafd, $forvalt, $place, $name) {
-		// Does the machine already exist in the database?
-		$statement = 'SELECT * FROM machine WHERE contact = :contact AND mail = :mail AND magafd = :magafd AND forvalt = :forvalt AND place = :place AND name = :name';
-		$query = $this->connection->execute($statement, array(	'contact' 	=> $contact,
-																'mail' 		=> $mail,
-																'magafd' 	=> $magafd,
-																'forvalt' 	=> $forvalt,
-																'place' 	=> $place,
-																'name' 		=> $name));
-		$count = $query->rowCount();
-		if ($count > 0) {
-		  // The machine already exists.
-			$result = array('result'=>'error', 'msg'=>'error_machine_already_exists');
-			echo json_encode($result);
-			return;
-		}
-	
-		// Make the macid from the first letter of "contant", "magafd", "forvalt", "place", "name" and next id in the machine table.
-		$macid = mb_substr($contact, 0, 1, "utf-8") . mb_substr($magafd, 0, 1, "utf-8") . mb_substr($forvalt, 0, 1, "utf-8") . mb_substr($place, 0, 1, "utf-8") . mb_substr($name, 0, 1, "utf-8");
-		$statement = "SELECT * FROM machine ORDER BY id DESC LIMIT 1";
-		$query = $this->connection->execute($statement);
-		$id = 1;
-		if ($query->rowCount() > 0) {
-			$row = $query->fetch();
-			$id = $row["id"] + 1;
-		}
-		$macid = $macid . $id;
+    // Does the machine already exist in the database?
+    $statement = 'SELECT * FROM machine WHERE contact = :contact AND mail = :mail AND magafd = :magafd AND forvalt = :forvalt AND place = :place AND name = :name';
+    $query = $this->connection->execute($statement, array(	'contact' 	=> $contact,
+      'mail' 		=> $mail,
+      'magafd' 	=> $magafd,
+      'forvalt' 	=> $forvalt,
+      'place' 	=> $place,
+      'name' 		=> $name)
+    );
+    $count = $query->rowCount();
+    if ($count > 0) {
+      // The machine already exists.
+      $result = array('result'=>'error', 'msg'=>'error_machine_already_exists');
+      echo json_encode($result);
+      return;
+    }
+
+    // Make the macid from the first letter of "contant", "magafd", "forvalt", "place", "name" and next id in the machine table.
+    $macid = mb_substr($contact, 0, 1, "utf-8") . mb_substr($magafd, 0, 1, "utf-8") . mb_substr($forvalt, 0, 1, "utf-8") . mb_substr($place, 0, 1, "utf-8") . mb_substr($name, 0, 1, "utf-8");
+    $statement = "SELECT * FROM machine ORDER BY id DESC LIMIT 1";
+    $query = $this->connection->execute($statement);
+    $id = 1;
+    if ($query->rowCount() > 0) {
+      $row = $query->fetch();
+      $id = $row["id"] + 1;
+    }
+    $macid = $macid . $id;
 
     // Replace the danish characters with english substitutes.
-		$macid = mb_ereg_replace("Ø","OE",$macid);
-		$macid = mb_ereg_replace("ø","oe",$macid);
-		$macid = mb_ereg_replace("Å","AA",$macid);
-		$macid = mb_ereg_replace("å","aa",$macid);
-		$macid = mb_ereg_replace("Æ","AE",$macid);
-		$macid = mb_ereg_replace("æ","ae",$macid);
+    $macid = mb_ereg_replace("Ø","OE",$macid);
+    $macid = mb_ereg_replace("ø","oe",$macid);
+    $macid = mb_ereg_replace("Å","AA",$macid);
+    $macid = mb_ereg_replace("å","aa",$macid);
+    $macid = mb_ereg_replace("Æ","AE",$macid);
+    $macid = mb_ereg_replace("æ","ae",$macid);
 
-		// Insert the new machine.
-		$statement = 'INSERT INTO machine (macid, contact, mail, magafd, forvalt, place, name) VALUES (:macid, :contact, :mail, :magafd, :forvalt, :place, :name)';
-		$query = $this->connection->execute($statement, array(	'macid' 	=> $macid,
-																'contact' 	=> $contact, 
-																'mail' 		=> $mail, 
-																'magafd' 	=> $magafd, 
-																'forvalt' 	=> $forvalt, 
-																'place' 	=> $place, 
-																'name' 		=> $name));
+    // Insert the new machine.
+    $statement = 'INSERT INTO machine (macid, contact, mail, magafd, forvalt, place, name) VALUES (:macid, :contact, :mail, :magafd, :forvalt, :place, :name)';
+    $query = $this->connection->execute($statement, array(	'macid' 	=> $macid,
+      'contact' 	=> $contact,
+      'mail' 		=> $mail,
+      'magafd' 	=> $magafd,
+      'forvalt' 	=> $forvalt,
+      'place' 	=> $place,
+      'name' 		=> $name)
+    );
 		
-		// Send email to the contact.
-		$to  = $mail; 
-		$subject = 'b7 oprettelse';
-		$message = '
-		<html>
-		<head>
-		  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		  <title>b7 oprettelse</title>
-		</head>
-		<body>
-		  <h2>Følgende maskine er blevet oprettet:</h2>
-		  <em>Login (macid)</em>: '.$macid.'<br/>
-		  <em>Magistratsafdeling</em>: '.$magafd.'<br/>
-		  <em>Forvaltning</em>: '.$forvalt.'<br/>
-		  <em>Fysisk placering</em>: '.$place.'<br/>
-		  <em>Navn på enhed</em>: '.$name.'<br/>
-		  <h2>Statistik</h2>
-		  Statistikken for den pågældende maskine er tilgængelig fra følgende link:<br/>
-		  <a href="http://smiley.aakb.dk/stats/?macid='.$macid.'">http://smiley.aakb.dk/stats/?macid='.$macid.'</a><br/>
-		  <br/>
-		  Der vil fremover blive sent en ugentlig mail (hver mandag) med et link til den foregående uges statistik.
-		</body>
-		</html>
-		';
-		$headers  = 'MIME-Version: 1.0' . "\r\n";
-		$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-		$headers .= 'From: b7' . "\r\n";		
-		mail($to, $subject, $message, $headers);
-		
-		// Return the macid to the client
-		$result = array('result'=>'ok', 'macid'=>$macid);
-		echo json_encode($result);	
-	}
+    // Send email to the contact.
+    $to  = $mail;
+    $subject = 'b7 oprettelse';
+    $message = '
+    <html>
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <title>b7 oprettelse</title>
+    </head>
+    <body>
+      <h2>Følgende maskine er blevet oprettet:</h2>
+      <em>Login (macid)</em>: '.$macid.'<br/>
+      <em>Magistratsafdeling</em>: '.$magafd.'<br/>
+      <em>Forvaltning</em>: '.$forvalt.'<br/>
+      <em>Fysisk placering</em>: '.$place.'<br/>
+      <em>Navn på enhed</em>: '.$name.'<br/>
+      <h2>Statistik</h2>
+      Statistikken for den pågældende maskine er tilgængelig fra følgende link:<br/>
+      <a href="http://smiley.aakb.dk/stats/?macid='.$macid.'">http://smiley.aakb.dk/stats/?macid='.$macid.'</a><br/>
+      <br/>
+      Der vil fremover blive sent en ugentlig mail (hver mandag) med et link til den foregående uges statistik.
+    </body>
+    </html>
+    ';
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+    $headers .= 'From: b7' . "\r\n";
+    mail($to, $subject, $message, $headers);
+
+    // Return the macid to the client
+    $result = array('result'=>'ok', 'macid'=>$macid);
+    echo json_encode($result);
+  }
 
   /**
    * Insert a result in the database.
@@ -144,39 +146,39 @@ class SmileyDB {
    * @param $what the reason for the smiley.
    */
   public function insertResult($macid, $datetime, $smiley, $what) {
-		// Test for existing macid.
-		$statement = 'SELECT * FROM machine WHERE macid = :macid';
-		$query = $this->connection->execute($statement, array('macid' => $macid));
-		if ($query->rowCount() != 1) {		// machine does not exist
-			$result = array('result'=>'error', 'msg'=>'error_machine_already_exists');
-			echo json_encode($result);
-			return;
-		}
-		
-		// Insert the new result into the database.
-		$statement = 'INSERT INTO data (macid, datetime, smiley, what) VALUES (:macid, :datetime, :smiley, :what)';
-		$query = $this->connection->execute($statement, array('macid' => $macid,
+    // Test for existing macid.
+    $statement = 'SELECT * FROM machine WHERE macid = :macid';
+    $query = $this->connection->execute($statement, array('macid' => $macid));
+    if ($query->rowCount() != 1) {		// machine does not exist
+      $result = array('result'=>'error', 'msg'=>'error_machine_already_exists');
+      echo json_encode($result);
+      return;
+    }
+
+    // Insert the new result into the database.
+    $statement = 'INSERT INTO data (macid, datetime, smiley, what) VALUES (:macid, :datetime, :smiley, :what)';
+    $query = $this->connection->execute($statement, array('macid' => $macid,
       'datetime' => $datetime,
       'smiley' => $smiley,
       'what' => $what)
     );
-																
-		$result = array('result'=>'ok');
-		echo json_encode($result);															
-	}
+
+    $result = array('result'=>'ok');
+    echo json_encode($result);
+  }
 
   /**
    * Returns all data for $macid, grouped by day.
    * @param $macid the macid of the machine to get data for.
    */
   public function getDataPerDay($macid) {
-		$statement = 'SELECT avg(smiley) as AvgSmiley, DATE(FROM_UNIXTIME(datetime/1000, "%Y-%m-%d")) as Date FROM data WHERE macid = :macid GROUP BY Date';
-		
-		$query = $this->connection->execute($statement, array('macid' => $macid));
-		
-		$rows = $query->fetchAll(PDO::FETCH_ASSOC);
-		echo json_encode($rows);
-	}
+    $statement = 'SELECT avg(smiley) as AvgSmiley, DATE(FROM_UNIXTIME(datetime/1000, "%Y-%m-%d")) as Date FROM data WHERE macid = :macid GROUP BY Date';
+
+    $query = $this->connection->execute($statement, array('macid' => $macid));
+
+    $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($rows);
+  }
 
   /**
    * Return data for $macid from the period from $start to $end. The data is number of the different smileys (1-5) for each what (1-3).
@@ -205,9 +207,9 @@ class SmileyDB {
       }
       array_push($arr, $insidearr);
     }
-		
-		return $arr;
-	}
+
+    return $arr;
+  }
 
   /**
    * Outputs the data for $macid from the period of one week ago until $today.
@@ -215,13 +217,13 @@ class SmileyDB {
    * @param $today the latest timestamp (milliseconds since Jan. 1, 1970) to include in the result.
    */
   public function getWhatThisWeek($macid, $today) {
-		$aDay = 1000 * 60 * 60 * 24;
-		$oneWeekAgo = $today -  $aDay * 7;
-		
-		$thisWeek = $this->getWhat($macid, $oneWeekAgo, $today);
-		
-		echo json_encode($thisWeek);
-	}
+    $aDay = 1000 * 60 * 60 * 24;
+    $oneWeekAgo = $today -  $aDay * 7;
+
+    $thisWeek = $this->getWhat($macid, $oneWeekAgo, $today);
+
+    echo json_encode($thisWeek);
+  }
 
   /**
    * Outputs the data from the beginning of time (January 1, 1970) until $end
@@ -229,60 +231,60 @@ class SmileyDB {
    * @param $end the latest timestamp (milliseconds since Jan. 1, 1970) to include in the result.
    */
   public function getWhatPast($macid, $end) {
-		$past = $this->getWhat($macid, 0, $end);
-		
-		echo json_encode($past);
-	}
+    $past = $this->getWhat($macid, 0, $end);
+
+    echo json_encode($past);
+  }
 
   /**
    * Send a mail to each registered machine with a link to the statistics from last week.
    */
   public function sendWeeklyMails() {
     // Get week and year of now.
-		$week = 0 + date("W");
-		$year = 0 + date("Y");
+    $week = 0 + date("W");
+    $year = 0 + date("Y");
 
-		// Count time one week back.
-		$week = $week - 1;
-		if ($week < 1) {
-			$week = 52;
-			$year = $year - 1;
-		}
+    // Count time one week back.
+    $week = $week - 1;
+    if ($week < 1) {
+      $week = 52;
+      $year = $year - 1;
+    }
 
     // Get all the machines from the database.
-		$statement = 'SELECT * FROM machine';
-		$query = $this->connection->execute($statement);
-		$rows = $query->fetchAll(PDO::FETCH_ASSOC);
+    $statement = 'SELECT * FROM machine';
+    $query = $this->connection->execute($statement);
+    $rows = $query->fetchAll(PDO::FETCH_ASSOC);
 
     // Send a mail with last weeks statistics to each machine's contact.
     foreach ($rows as $row) {
-			$to  = $row["mail"];
-			$subject = 'b7 statistik uge '.$week.', '.$year;
-			$message = '
-			<html>
-			<head>
-			    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-			    <title>b7 ugentlig statistik for uge '.$week.'('.$year.')</title>
-			</head>
-			<body>
-			    <div>
-                    <h2>Statistik for uge '.$week.' ('.$year.')</h2>
-                    <a href="http://smiley.aakb.dk/stats/?macid='.$row["macid"].'&week='.$week.'&year='.$year.'">http://smiley.aakb.dk/stats/?macid='.$row["macid"].'&week='.$week.'&year='.$year.'</a>
-                    <h2>For følgende maskine</h2>
-                    <em>Magistratsafdeling</em>: '.$row["magafd"].'<br/>
-                    <em>Forvaltning</em>: '.$row["forvalt"].'<br/>
-                    <em>Fysisk placering</em>: '.$row["place"].'<br/>
-                    <em>Navn på enhed</em>: '.$row["name"].'<br/>
-			    </div>
-			</body>
-			</html>
-			';
-			$headers  = 'MIME-Version: 1.0' . "\r\n";
-			$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-			$headers .= 'From: b7' . "\r\n";		
-			mail($to, $subject, $message, $headers);
-		}
-	}
+      $to  = $row["mail"];
+      $subject = 'b7 statistik uge '.$week.', '.$year;
+      $message = '
+      <html>
+      <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>b7 ugentlig statistik for uge '.$week.'('.$year.')</title>
+      </head>
+      <body>
+        <div>
+          <h2>Statistik for uge '.$week.' ('.$year.')</h2>
+          <a href="http://smiley.aakb.dk/stats/?macid='.$row["macid"].'&week='.$week.'&year='.$year.'">http://smiley.aakb.dk/stats/?macid='.$row["macid"].'&week='.$week.'&year='.$year.'</a>
+          <h2>For følgende maskine</h2>
+          <em>Magistratsafdeling</em>: '.$row["magafd"].'<br/>
+          <em>Forvaltning</em>: '.$row["forvalt"].'<br/>
+          <em>Fysisk placering</em>: '.$row["place"].'<br/>
+          <em>Navn på enhed</em>: '.$row["name"].'<br/>
+        </div>
+      </body>
+      </html>
+      ';
+      $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+      $headers .= 'From: b7' . "\r\n";
+      mail($to, $subject, $message, $headers);
+    }
+  }
 
   /**
    * Outputs an XML document of all the data.
