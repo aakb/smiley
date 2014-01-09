80d6c6b6235ee3d05984d1946b46b17da23b3706
Troels Ugilt Jensen <tujensen@gmail.com>
2014-01-09 14:44:51 +0100

    indention

--- client/js/index.js
+++ client/js/index.js
@@ -96,235 +96,235 @@ var app = {
         updateButtonText(commitButton);
       });
     });
-	},
+  },
 
   /**
    * Shows the register page and sets up event listeners.
    */
-	showRegisterPage: function() {
-		// Change the HTML content.
-		$("#main").html(app.pageRegister);
+  showRegisterPage: function() {
+    // Change the HTML content.
+    $("#main").html(app.pageRegister);
 
-		// Setup custom validation for the email_repeat input.
-		$("#mail_repeat").on("change", function() {
-			if ($(this).val() !== $("#mail").first().val()) {
-				document.getElementById("mail_repeat").setCustomValidity("Emails er ikke ens");
-			}
+    // Setup custom validation for the email_repeat input.
+    $("#mail_repeat").on("change", function() {
+      if ($(this).val() !== $("#mail").first().val()) {
+        document.getElementById("mail_repeat").setCustomValidity("Emails er ikke ens");
+      }
       else {
-				document.getElementById("mail_repeat").setCustomValidity("");
-			}
-		});
+        document.getElementById("mail_repeat").setCustomValidity("");
+      }
+    });
 
-		// Setup form register submit button
-		$("#form_register").submit(function(event) {
-			event.preventDefault();
+    // Setup form register submit button
+    $("#form_register").submit(function(event) {
+      event.preventDefault();
 
       // Serialize the input data from form.
-			var serializedData = $(this).serializeArray();
-			
-			// Disable inputs during the ajax request.
-			var $inputs = $(this).find("input");
-			$inputs.prop("disabled", true);
+      var serializedData = $(this).serializeArray();
 
-			// Post the data to the server.
-			$.ajax({
-				url: config.serverlocation,
-				type: "POST",
-				data: serializedData,
-				dataType: "json"
-			})
-			.done(function (response, textStatus, jqXHR) {
+      // Disable inputs during the ajax request.
+      var $inputs = $(this).find("input");
+      $inputs.prop("disabled", true);
+
+      // Post the data to the server.
+      $.ajax({
+        url: config.serverlocation,
+        type: "POST",
+        data: serializedData,
+        dataType: "json"
+      })
+      .done(function (response, textStatus, jqXHR) {
         var resp = JSON.parse(JSON.stringify(response));
 
-				if (resp.result == "ok") {
-					// Set the app.macid and save to local storage for auto-login.
+        if (resp.result == "ok") {
+          // Set the app.macid and save to local storage for auto-login.
           app.macid = resp.macid;
-					app.saveMacidToLocalStorage(app.macid);
+          app.saveMacidToLocalStorage(app.macid);
 
           // Show alert to user of success and with the macid of the registered machine.
-					alert("Registreringen lykkedes!\r\nmacid til denne opsætning er \r\n" + app.macid + "\r\nSkriv den ned, så du har den til næste gange du skal logge denne maskine ind. De indtastede informationer og også sendt til den registrerede email-adresse.");
+          alert("Registreringen lykkedes!\r\nmacid til denne opsætning er \r\n" + app.macid + "\r\nSkriv den ned, så du har den til næste gange du skal logge denne maskine ind. De indtastede informationer og også sendt til den registrerede email-adresse.");
 
           // Go to main page.
           app.showMainPage();
-				} else if (resp.result == "error") {
-					if (resp.msg == "error_machine_already_exists") {
-						alert("Fejl! Den maskine er allerede oprettet.");
-					}
+        } else if (resp.result == "error") {
+          if (resp.msg == "error_machine_already_exists") {
+            alert("Fejl! Den maskine er allerede oprettet.");
+          }
           else {
-						alert("Der skete en fejl. Prøv igen!");
-					}
-				}
-			})
-			.fail(function (jqXHR, textStatus, errorThrown){
-				alert("Der skete en fejl. Prøv igen! Dette skyldes formentlig manglende internetforbindelse eller at serveren ikke kører. " + errorThrown);
-			})
-			.always(function () {
+            alert("Der skete en fejl. Prøv igen!");
+          }
+        }
+      })
+      .fail(function (jqXHR, textStatus, errorThrown){
+        alert("Der skete en fejl. Prøv igen! Dette skyldes formentlig manglende internetforbindelse eller at serveren ikke kører. " + errorThrown);
+      })
+      .always(function () {
         // Reenable the inputs.
-				$inputs.prop("disabled", false);
-			});
-		});
-	},
+        $inputs.prop("disabled", false);
+      });
+    });
+  },
 
   /**
    * Shows the login page and sets event listeners.
    */
-	showLoginPage: function() {
-		// Change HTML content
-		$("#main").html(app.pageLogin);
+  showLoginPage: function() {
+    // Change HTML content
+    $("#main").html(app.pageLogin);
 
-		// Setup form register submit button
-		$("#form_login").submit(function(event) {
-			event.preventDefault();
-			
-			// Get the macid from the input.
-			var macid = $("#macid").val();
-			
-			// Disable inputs during the ajax request.
-			var $inputs = $(this).find("input");
-			$inputs.prop("disabled", true);         
+    // Setup form register submit button
+    $("#form_login").submit(function(event) {
+      event.preventDefault();
 
-			// Post the login data to the server.
-			$.ajax({
-				url: config.serverlocation,
-				type: "POST",
-				data: {action: "login", macid: macid},
-				dataType: "json"
-			})
-			.done(function (response, textStatus, jqXHR){
-				var resp = JSON.parse(JSON.stringify(response));
-				if (resp.result == "ok") {
+      // Get the macid from the input.
+      var macid = $("#macid").val();
+
+      // Disable inputs during the ajax request.
+      var $inputs = $(this).find("input");
+      $inputs.prop("disabled", true);
+
+      // Post the login data to the server.
+      $.ajax({
+        url: config.serverlocation,
+        type: "POST",
+        data: {action: "login", macid: macid},
+        dataType: "json"
+      })
+      .done(function (response, textStatus, jqXHR){
+        var resp = JSON.parse(JSON.stringify(response));
+        if (resp.result == "ok") {
           // Set the app.macid and save to local storage for auto-login.
- 					app.macid = macid;
-					app.saveMacidToLocalStorage(macid);
+          app.macid = macid;
+          app.saveMacidToLocalStorage(macid);
 
           // Go to the main page.
-					app.showMainPage();
-				}
+          app.showMainPage();
+        }
         else if (resp.result == "error") {
-					if (resp.msg == "error_wrong_id") {
-						alert("Fejl! Ukendt ID.");
-					}
+          if (resp.msg == "error_wrong_id") {
+            alert("Fejl! Ukendt ID.");
+          }
           else {
-						alert("Der skete en fejl. Prøv igen!");
-					}
-				}
-			})
-			.fail(function (jqXHR, textStatus, errorThrown){
-				alert("Der skete en fejl. Prøv igen! Dette skyldes formentlig manglende internetforbindelse eller at serveren ikke kører.");
-			})
-			.always(function () {
+            alert("Der skete en fejl. Prøv igen!");
+          }
+        }
+      })
+      .fail(function (jqXHR, textStatus, errorThrown){
+        alert("Der skete en fejl. Prøv igen! Dette skyldes formentlig manglende internetforbindelse eller at serveren ikke kører.");
+      })
+      .always(function () {
         // Reenable the inputs.
-				$inputs.prop("disabled", false);
-			});
-		});
-	},
+        $inputs.prop("disabled", false);
+      });
+    });
+  },
 
   /**
    * Shows the main page and registers event listeners.
    */
-	showMainPage: function() {
-		// Clear timers and event handlers.
-		app.clearClickHandlers();
-		clearTimeout(app.timer);
-		
-		// Change the HTML content.
-		$("#main").html(app.pageMain);
-		
-		// Hide bottom images.
-		$(".img_choice").hide();
-		
-		// Set text
-		$("#table_text").html("<h1>Tilfreds med betjeningen?</h1>");
-		
-		// Setup the event listeners.
-		$("#smiley1").on("touchstart click", function(e) {
-			e.stopPropagation(); e.preventDefault();
-			app.showWhatPage(1);
-		});
-		$("#smiley2").on("touchstart click", function(e) {
-			e.stopPropagation(); e.preventDefault();
-			app.showWhatPage(2);
-		});
-		$("#smiley3").on("touchstart click", function(e) {
-			e.stopPropagation(); e.preventDefault();
-			app.showWhatPage(3);
-		});
-		$("#smiley4").on("touchstart click", function(e) {
-			e.stopPropagation(); e.preventDefault();
-			app.showWhatPage(4);
-		});
-		$("#smiley5").on("touchstart click", function(e) {
-			e.stopPropagation(); e.preventDefault();
-			app.showWhatPage(5);
-		});
-	},
+  showMainPage: function() {
+    // Clear timers and event handlers.
+    app.clearClickHandlers();
+    clearTimeout(app.timer);
+
+    // Change the HTML content.
+    $("#main").html(app.pageMain);
+
+    // Hide bottom images.
+    $(".img_choice").hide();
+
+    // Set text
+    $("#table_text").html("<h1>Tilfreds med betjeningen?</h1>");
+
+    // Setup the event listeners.
+    $("#smiley1").on("touchstart click", function(e) {
+      e.stopPropagation(); e.preventDefault();
+      app.showWhatPage(1);
+    });
+    $("#smiley2").on("touchstart click", function(e) {
+      e.stopPropagation(); e.preventDefault();
+      app.showWhatPage(2);
+    });
+    $("#smiley3").on("touchstart click", function(e) {
+      e.stopPropagation(); e.preventDefault();
+      app.showWhatPage(3);
+    });
+    $("#smiley4").on("touchstart click", function(e) {
+      e.stopPropagation(); e.preventDefault();
+      app.showWhatPage(4);
+    });
+    $("#smiley5").on("touchstart click", function(e) {
+      e.stopPropagation(); e.preventDefault();
+      app.showWhatPage(5);
+    });
+  },
 
   /**
    * Shows the what page, registers event listeners and timer.
    * @param nSmiley The selected smiley from main page.
    */
-	showWhatPage: function(nSmiley) {
+  showWhatPage: function(nSmiley) {
     // Clear timers and event handlers.
-		app.clearClickHandlers();
-		clearTimeout(app.timer);
+    app.clearClickHandlers();
+    clearTimeout(app.timer);
 
-		// Change the HTML content.
-		$(".main").html(app.pageMain);
-		
-		// Setup event listeners
-		$("#choice1").on("touchstart click", function(e) {
-			e.stopPropagation(); e.preventDefault();
-			app.showResultPage(nSmiley, 1);
-		});
-		$("#choice2").on("touchstart click", function(e) {
-			e.stopPropagation(); e.preventDefault();
-			app.showResultPage(nSmiley, 2);
-		});
-		$("#choice3").on("touchstart click", function(e) {
-			e.stopPropagation(); e.preventDefault();
-			app.showResultPage(nSmiley, 3);
-		});
+    // Change the HTML content.
+    $(".main").html(app.pageMain);
 
-		// Change the text according to the selected smiley.
-		if (nSmiley > 3) {
-			$("#table_text").html("<h1>Hvad var godt?</h1>");
-		} else {
-			$("#table_text").html("<h1>Hvad var dårligt?</h1>");
-		}
-		
-		// Show the selected smiley and hide the others.
-		$(".img_smiley").each(function(index) {
-			if (index == 4 - (nSmiley - 1)) {
+    // Setup event listeners
+    $("#choice1").on("touchstart click", function(e) {
+      e.stopPropagation(); e.preventDefault();
+      app.showResultPage(nSmiley, 1);
+    });
+    $("#choice2").on("touchstart click", function(e) {
+      e.stopPropagation(); e.preventDefault();
+      app.showResultPage(nSmiley, 2);
+    });
+    $("#choice3").on("touchstart click", function(e) {
+      e.stopPropagation(); e.preventDefault();
+      app.showResultPage(nSmiley, 3);
+    });
+
+    // Change the text according to the selected smiley.
+    if (nSmiley > 3) {
+      $("#table_text").html("<h1>Hvad var godt?</h1>");
+    } else {
+      $("#table_text").html("<h1>Hvad var dårligt?</h1>");
+    }
+
+    // Show the selected smiley and hide the others.
+    $(".img_smiley").each(function(index) {
+      if (index == 4 - (nSmiley - 1)) {
         $(this).addClass("img_smiley_selected");
       }
-			else {
+      else {
         $(this).addClass("img_smiley_hide");
       }
-		});
+    });
 
-		// Set timeout for page. If user has not selected a reason for the smiley before the timeout, then load main page again.
-		app.timer = setTimeout(function(){
-			app.showMainPage();
-		}, 10000);
-	},
+    // Set timeout for page. If user has not selected a reason for the smiley before the timeout, then load main page again.
+    app.timer = setTimeout(function(){
+      app.showMainPage();
+    }, 10000);
+  },
 
   /**
    * Shows the result page.
    * @param nSmiley the selected smiley.
    * @param nWhat   the reason for the smiley.
    */
-	showResultPage: function(nSmiley, nWhat) {
+  showResultPage: function(nSmiley, nWhat) {
     // Clear event handlers and timer.
-		app.clearClickHandlers();
-		clearTimeout(app.timer);
+    app.clearClickHandlers();
+    clearTimeout(app.timer);
 
     // Set the HTML content.
-		$("#main").html(app.pageThanks);
+    $("#main").html(app.pageThanks);
 
     var datetime = (new Date()).getTime();
 
-		// Post data to server
-		app.sendResultToServer(app.macid, nSmiley, nWhat, datetime, function() {
+    // Post data to server
+    app.sendResultToServer(app.macid, nSmiley, nWhat, datetime, function() {
       // Test if the connection to the server is up
       app.ping(
         // If the connection is up, commit from local storage.
@@ -341,8 +341,8 @@ var app = {
           }, 4000);
         }
       );
-		});
-	},
+    });
+  },
 
   /**
    * Sends a single result to the server.
@@ -352,71 +352,71 @@ var app = {
    * @param datetime the time of the result.
    * @param callback the function to call when the function is done.
    */
-	sendResultToServer: function(macid, smiley, what, datetime, callback) {
+  sendResultToServer: function(macid, smiley, what, datetime, callback) {
     // Send the result to the server.
-		$.ajax({
-			url: config.serverlocation,
-			type: "POST",
-			data: {action: "result", macid: macid, smiley: smiley, what: what, datetime: datetime},
-			dataType: "json"
-		})
-		.done(function (response, textStatus, jqXHR) {
-			var resp = JSON.parse(JSON.stringify(response));
+    $.ajax({
+      url: config.serverlocation,
+      type: "POST",
+      data: {action: "result", macid: macid, smiley: smiley, what: what, datetime: datetime},
+      dataType: "json"
+    })
+    .done(function (response, textStatus, jqXHR) {
+      var resp = JSON.parse(JSON.stringify(response));
 
-			if (resp.result != "ok") {
+      if (resp.result != "ok") {
         // If an error occurred, save the entry to local storage.
-				app.saveEntryToLocalStorage(macid, smiley, what, datetime);
-			}
-			callback();
-		})
-		.fail(function (jqXHR, textStatus, errorThrown) {
+        app.saveEntryToLocalStorage(macid, smiley, what, datetime);
+      }
+      callback();
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
       // When the commit fails, save to local storage.
-			app.saveEntryToLocalStorage(macid, smiley, what, datetime);
-			callback();
-		});
-	},
+      app.saveEntryToLocalStorage(macid, smiley, what, datetime);
+      callback();
+    });
+  },
 
   /**
    * Recursively commit the list.
    * @param list the list of results to commit.
    * @param callback the function to call when the list is empty.
    */
-	// Commit single entry, recursive until empty list
-	commitListRecurse: function(list, callback) {
-		// The stop condition is an empty list.
+  // Commit single entry, recursive until empty list
+  commitListRecurse: function(list, callback) {
+    // The stop condition is an empty list.
     if (list.length > 0) {
       // Pop top element of list.
-			var ent = list.pop();
+      var ent = list.pop();
 
       // Commit the entry and then recursively commit the rest of the list.
-			app.sendResultToServer(ent.macid, ent.smiley, ent.what, ent.datetime, function() {
-				app.commitListRecurse(list, callback);
-			});
-		} else {
-			callback();
-		}
-	},
+      app.sendResultToServer(ent.macid, ent.smiley, ent.what, ent.datetime, function() {
+        app.commitListRecurse(list, callback);
+      });
+    } else {
+      callback();
+    }
+  },
 
   /**
    * Commit the results that have not been sent.
    * @param callback the function to call when the list is empty.
    */
-	commitEntriesFromLocalStorage: function(callback) {
-		if(typeof(Storage) !== "undefined") {
-			// Get local storage entries
-			var entries = JSON.parse(localStorage.getItem("entries"));
+  commitEntriesFromLocalStorage: function(callback) {
+    if(typeof(Storage) !== "undefined") {
+      // Get local storage entries
+      var entries = JSON.parse(localStorage.getItem("entries"));
 
-			// Clear local storage entries
-			localStorage.setItem("entries", JSON.stringify([]));
+      // Clear local storage entries
+      localStorage.setItem("entries", JSON.stringify([]));
 
       // Recursively commit the list
-			if (entries !== null) {
-				app.commitListRecurse(entries, callback);
-			} else {
-				callback();
-			}
-		}
-	},
+      if (entries !== null) {
+        app.commitListRecurse(entries, callback);
+      } else {
+        callback();
+      }
+    }
+  },
 
   /**
    * Save a result to local storage.
@@ -425,26 +425,26 @@ var app = {
    * @param what the reason for the selected smiley.
    * @param datetime the time of the result.
    */
-	// Save an entry to local storage
-	saveEntryToLocalStorage: function(macid, smiley, what, datetime) {
-		if(typeof(Storage) !== "undefined") {
-			var ent = {
-				macid: macid,
-				smiley: smiley,
-				what: what,
-				datetime: datetime
-			}
-		
-			var entries = JSON.parse(localStorage.getItem("entries"));
-		
-			if (entries == null) {
-				entries = [];
-			} 
-			entries.push(ent);
+  // Save an entry to local storage
+  saveEntryToLocalStorage: function(macid, smiley, what, datetime) {
+    if(typeof(Storage) !== "undefined") {
+      var ent = {
+        macid: macid,
+        smiley: smiley,
+        what: what,
+        datetime: datetime
+      }
 
-			localStorage.setItem("entries", JSON.stringify(entries));
-		}
-	},
+      var entries = JSON.parse(localStorage.getItem("entries"));
+
+      if (entries == null) {
+        entries = [];
+      }
+      entries.push(ent);
+
+      localStorage.setItem("entries", JSON.stringify(entries));
+    }
+  },
 
   /**
    * Ping the server.
@@ -467,26 +467,26 @@ var app = {
   /**
    * Removes the macid from local storage.
    */
-	logout: function() {
+  logout: function() {
     if(typeof(Storage) !== "undefined") {
-		  localStorage.removeItem("macid");
+      localStorage.removeItem("macid");
     }
-	},
+  },
 
   /**
    * Save the macid to local storage.
    * @param macid
    */
-	saveMacidToLocalStorage: function(macid) {
-		if(typeof(Storage) !== "undefined") {
-			localStorage.setItem("macid", macid);
-		}
-	},
-	
+  saveMacidToLocalStorage: function(macid) {
+    if(typeof(Storage) !== "undefined") {
+      localStorage.setItem("macid", macid);
+    }
+  },
+
   /**
    * Clear the event handlers for the smileys and the whats.
    */
-	clearClickHandlers: function() {
-		$("#smiley1 #smiley2 #smiley3 #smiley4 #smiley5 #choice1 #choice2 #choice3").off();
-	}
+  clearClickHandlers: function() {
+    $("#smiley1 #smiley2 #smiley3 #smiley4 #smiley5 #choice1 #choice2 #choice3").off();
+  }
 };
